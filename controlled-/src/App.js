7e4fecea3dcddb0852f3fab3e15124dd83c0a216
meetmarkana <meetmarakna2078@gmail.com>
2024-04-22 18:45:32 +0530

    Controlled & Uncontrolled

---
+++ controlled-/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Form';
+import Controlled from './Controlled';
+function App() {
+  return (
+    <div className="App">
+      <center>
+      <Controlled/>
+      <Form/>
+      </center>
+      
+    </div>
+  );
+}
+
+export default App;
