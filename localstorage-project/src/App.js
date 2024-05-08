aa71e3c7ff307abb948f57b8a7277f48cd8fc503
meetmarkana <meetmarakna2078@gmail.com>
2024-05-08 18:19:29 +0530

    localstorage-project

---
+++ localstorage-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Data from './Data';
+
+function App() {
+  return (
+    <div className="App">
+      <Data/>
+    </div>
+  );
+}
+
+export default App;
---
+++ localstorage-project/src/Data.jsx
@@ -0,0 +1,69 @@
+import React,{useState} from 'react'
+
+function Data() {
+  const[moviename,setMoviename]=useState('')
+  const[rating,setRatings]=useState('')
+  const[moviedescription,setMovieDescription]=useState('')
+  const[movies,setMovies]=useState([])
+
+
+    localStorage.setItem("movies",JSON.stringify(movies))
+
+
+     function addMovie()
+     {
+        let newMovie=
+        {
+          Movie:moviename,
+          Description:moviedescription,
+          Ratings:rating
+        }
+
+        setMovies([...movies,newMovie])
+
+        setMoviename('')
+        setMovieDescription('')
+        setRatings('')
+      }
+  return (
+      <div><center>
+          <h1>Movies</h1>
+
+        <br /> <br />
+
+        <input type="text" placeholder='Enter Movie Name' value={moviename} onChange={(e)=>setMoviename(e.target.value)}/><br/><br/><br/>
+        <input type="text" placeholder='Enter Description' value={moviedescription} onChange={(e)=>setMovieDescription(e.target.value)}/><br/><br/><br/>
+        <input type="text" placeholder='Enter Ratings' value={rating} onChange={(e)=>setRatings(e.target.value)}/><br/><br/><br/>
+        <button onClick={addMovie}>Add</button>
+
+
+        <div className='main'>
+          <table border={3}>
+            <thead>
+              <tr>
+                <th>Movie Name</th>
+                <th>Movie Description</th>
+                <th>Rating</th>
+                
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map((el,i) => (
+                <tr key={i}>
+                  <td>{el.Movie}</td>
+                  <td>{el.Description}</td>
+                  <td>{el.Ratings}</td>
+                  
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+       
+  </center>
+    </div>
+    
+  )
+}
+
+export default Data
